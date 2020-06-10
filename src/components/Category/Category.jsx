f7716f3eabec395e9c2a8d284e618f27607d077d
Julio <jc.gutierrezq@gmail.com>
2020-06-10 16:34:52 -0500

    styles

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,8 +3,11 @@ import { Anchor, Image } from "./styles";
 
 const DEFAULT_IMAGE = "https://i.imgur.com/dJaHpl.jpg";
 
+// Componente nombrado
 export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = "?" }) => (
   <Anchor href={path}>
     <Image src={cover} />
+    {emoji}
   </Anchor>
 );
+
