4809c12158d607fa9ef2737b4a4e2a83418ac0b6
Julio <jc.gutierrezq@gmail.com>
2020-06-12 15:06:47 -0500

    react-icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { ListOfCategories } from "./components/ListOfCategories/index";
 import { GlobalStyle } from "./GlobalStyles";
+import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
 
 export const App = () => (
   <div>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </div>
 );
