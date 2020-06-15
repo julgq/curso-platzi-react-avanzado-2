9b73a055e2533a452f025f7c7991e9d088419086
Julio <jc.gutierrezq@gmail.com>
2020-06-14 21:25:00 -0500

    react hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Category } from "../Category/Category";
 import { List, Item } from "./styles";
-import { categories } from "../../db.json";
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  /* useEffect se ejecuta cada vez que se renderiza el componente */
+  useEffect(function () {
+    window
+      .fetch("http://localhost:3000/categories")
+      .then((res) => res.json())
+      .then((response) => {
+        setCategories(response);
+      });
+  }, []);
+
   return (
     <List>
       {categories.map((category) => (
