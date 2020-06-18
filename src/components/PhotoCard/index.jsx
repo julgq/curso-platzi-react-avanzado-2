4282021e379eab310a5b690672a556ec52271bab
Julio <jc.gutierrezq@gmail.com>
2020-06-18 16:30:59 -0600

    polyfill

---
+++ src/components/PhotoCard/index.jsx
@@ -0,0 +1,48 @@
+import React, { Fragment, useEffect, useRef, useState } from 'react'
+import { Article, ImgWrapper, Img, Button } from './styles'
+import { MdFavoriteBorder } from 'react-icons/md'
+
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+
+    Promise.resolve(
+      // Validar si InteresectionObserver es compatible con el navegador, si no es compatible se carga el import dinamico.
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          console.log(isIntersecting)
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
+    })
+  }, [element])
+  
+  return (
+    <Article ref={element}>
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </Fragment>
+      }
+    </Article>
+  )
+}
