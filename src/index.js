658aa6600f6f941f1af0ac8a18589216f1ae9a46
Julio <jc.gutierrezq@gmail.com>
2020-06-23 19:59:06 -0600

    api and graphql

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3500/graphql'
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
+    
   </React.StrictMode>,
   document.getElementById("root")
 );
